ea332b5dcff071c2f2a872d1f97c8c6f837b7c1a
ReemaGoveas1 <46667929+ReemaGoveas1@users.noreply.github.com>
2022-09-26 15:01:22 +0530

    Create Counter.svelte

---
+++ Counter.svelte
@@ -0,0 +1,21 @@
+//"cerner_2tothe5th_2022"
+
+<script>
+    let count = 0
+    function handleClick(event) {
+        console.log(event);
+        count += 1
+    }
+    function handleClickNext(event, stepSize) {
+        console.log(event);
+        count += stepSize
+    }
+</script>
+
+<main>
+    <!-- Inline event Handlers -->
+    <button on:click = {() => (count = count+1)}> Count {count}</button>
+    <button on:click = {handleClick}> Count {count}</button>
+    <!-- We can also specify the arguments -->
+    <button on:click = {(event) =>handleClickNext(event,5)}> Count {count}</button>
+</main>
