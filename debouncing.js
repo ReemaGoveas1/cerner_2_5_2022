46a467abe8823cae09b2ae3765c4d6fad8864d07
ReemaGoveas1 <46667929+ReemaGoveas1@users.noreply.github.com>
2022-09-30 08:29:28 +0530

    Create debouncing.js

---
+++ debouncing.js
@@ -0,0 +1,18 @@
+//The debounce function forces a function to wait a certain amount of time before running again. 
+//It is a practice used to improve browser performance.
+
+const fetchData = (event) =>{
+     console.log("Network call", event.target.value)
+ }
+
+ const debounce = function (wrapperFunction, delay){
+     let timmer;
+     return function(){
+         clearInterval(timmer);
+         timmer=setTimeout(() => {
+             wrapperFunction.apply(this, arguments) 
+         }, delay);
+     }
+ }
+
+ const enhancedDebounceFunction = debounce(fetchData, 2000); //calls fetch data only at certain interval of time
